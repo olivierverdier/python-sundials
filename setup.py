76a059f8240901ee09e4c22327b5537d3277b762
Olivier Verdier <olivier.verdier@gmail.com>
2011-09-28 11:51:32 +0200

    remove try/except block in setup.py

--- setup.py
+++ setup.py
@@ -23,50 +23,43 @@ Programming Language :: Python
 Topic :: Scientific/Engineering :: Mathematics
 '''
 
-try:
-    setup(
-      name = 'python-sundials',
-      version = '0.5',
-      description = 'Sundials solver library wrapper',
-      long_description = '''\
+setup(
+  name = 'python-sundials',
+  version = '0.5',
+  description = 'Sundials solver library wrapper',
+  long_description = '''\
 **python-sundials** is a Cython wrapper for the Sundials solver suite.
 The wrapper is based on code posten on the cython-dev mailing list by
 Mr. Jon Olav Vik.
 
 **Highlights**
 
- * CVODE - Solver for stiff and nonstiff ordinary differential equation
- * IDA - Solver for the solution of differential-algebraic equation (DAE) systems
- * KINSOL - solver for nonlinear algebraic systems based on Newton-Krylov solver technology
+* CVODE - Solver for stiff and nonstiff ordinary differential equation
+* IDA - Solver for the solution of differential-algebraic equation (DAE) systems
+* KINSOL - solver for nonlinear algebraic systems based on Newton-Krylov solver technology
 
 The CVODE and IDA solvers support root finding and the solver throws an exception
 on finding a root.
 ''',
-        
-    classifiers = [value for value in classifiers.split("\n") if value],
-        
-    author='Runar Tenfjord',
-    author_email = 'runar.tenfjord@google.com',
-    license = 'BSD',
-    download_url='http://pypi.python.org/pypi/python-sundials/',
-    url = 'http://code.google.com/p/python-sundials/',
-    platforms = ['any'],
     
-    ext_modules=[   
-        Extension("sundials",
-                  sources = ["sundials/sundials.pyx",],
-                  depends = glob.glob('sundials/*.pxi'),
-                  include_dirs = ['sundials', 'sundials/include'],
-                  library_dirs=['sundials'],
-                  libraries = ['sundials_cvode','sundials_ida',
-                               'sundials_kinsol', 'sundials_nvecserial'])
-        ],
-        
-      cmdclass = {'build_ext': build_ext}
-    )
-except:
-    print('Traceback\n:%s\n' % str(sys.exc_info()[-2]))
-else:
-    print('\n')
+classifiers = [value for value in classifiers.split("\n") if value],
     
-input('Press enter to continue')
+author='Runar Tenfjord',
+author_email = 'runar.tenfjord@google.com',
+license = 'BSD',
+download_url='http://pypi.python.org/pypi/python-sundials/',
+url = 'http://code.google.com/p/python-sundials/',
+platforms = ['any'],
+
+ext_modules=[   
+    Extension("sundials",
+              sources = ["sundials/sundials.pyx",],
+              depends = glob.glob('sundials/*.pxi'),
+              include_dirs = ['sundials', 'sundials/include'],
+              library_dirs=['sundials'],
+              libraries = ['sundials_cvode','sundials_ida',
+                           'sundials_kinsol', 'sundials_nvecserial'])
+    ],
+    
+  cmdclass = {'build_ext': build_ext}
+)
